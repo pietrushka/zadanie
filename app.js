9d574bd9e51136b84d4f09afc357400039a51db0
pietrushka <piotrekwisnia18@gmail.com>
2021-02-04 11:47:26 +0100

    Add login logic

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+import axios from 'axios';
+
 import { route } from './router';
 
 route('/', 'home', function() {
-    this.title = 'Home';
+    this.error = ''
+
+    this.$on('#login-form', 'submit', async (event) => {
+        event.preventDefault();
+        const email = event.target.elements['email'].value;
+        const password = event.target.elements['password'].value;
+
+        try {
+            const {status, data} = await axios.post('https://zwzt-zadanie.netlify.app/api/login', {email, password});
+            window.location.href = '#/success';
+        } catch (error) {
+            this.error = 'Something went wrong, please try again';
+            this.$refresh();
+        };
+    });
+    
 });
 
 
